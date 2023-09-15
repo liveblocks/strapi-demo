a6edce3234f802617dd1226b9ac28a9e4aa2484a
CTNicholas <33033422+CTNicholas@users.noreply.github.com>
2023-09-15 13:23:38 +0100

    feat: Temp fix for z-index issues

--- nextjs-blog/src/components/assorted/Room.tsx
+++ nextjs-blog/src/components/assorted/Room.tsx
@@ -19,7 +19,7 @@ export function Room({ children }: Props) {
   return (
     <RoomProvider
       // id={"http://localhost:3000" + pathname}
-      id="my-temporary-room-name-3"
+      id="my-temporary-room-name-4"
       initialPresence={{ cursor: null }}
     >
       {children}
