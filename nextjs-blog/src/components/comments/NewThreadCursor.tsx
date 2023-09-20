6485a1a5d0b8afb86136f3030fb3f9d3fd89fdf5
CTNicholas <33033422+CTNicholas@users.noreply.github.com>
2023-09-20 14:40:54 +0100

    feat: New thread cursor

--- nextjs-blog/src/components/comments/NewThreadCursor.tsx
+++ nextjs-blog/src/components/comments/NewThreadCursor.tsx
@@ -1,9 +1,6 @@
 import { useEffect, useState } from "react";
 import * as Portal from "@radix-ui/react-portal";
-import { Pointer, POINTER_OFFSET } from "./Pointer";
 import styles from "./NewThreadCursor.module.css";
-import stylesOverlay from "./Overlay.module.css";
-import { PlusCircleIcon } from "@/components/icons/PlusCircleIcon";
 
 export function NewThreadCursor({ display }: { display: boolean }) {
   const [coords, setCoords] = useState({
@@ -45,17 +42,9 @@ export function NewThreadCursor({ display }: { display: boolean }) {
       <div
         className={styles.newThreadCursor}
         style={{
-          transform: `translate(${coords.x + POINTER_OFFSET.x}px, ${
-            coords.y + POINTER_OFFSET.y
-          }px)`,
+          transform: `translate(${coords.x}px, ${coords.y}px)`,
         }}
-      >
-        <Pointer />
-        <div className={stylesOverlay.minimizedThread}>
-          <PlusCircleIcon opacity="0.7" />
-          <div>New thread</div>
-        </div>
-      </div>
+      />
     </Portal.Root>
   );
 }
