805db72e934e4a01f6aba4c3199d5ac309ae1f5a
CTNicholas <33033422+CTNicholas@users.noreply.github.com>
2023-09-20 14:34:54 +0100

    feat: Toolbar avatars

--- nextjs-blog/src/components/comments/Toolbar.tsx
+++ nextjs-blog/src/components/comments/Toolbar.tsx
@@ -5,17 +5,16 @@ import { List, More, Plus } from "@strapi/icons";
 import { useState } from "react";
 import * as Collapsible from "@radix-ui/react-collapsible";
 import { NewThread } from "@/components/comments/NewThread";
-import { AvatarStack } from "@/components/comments/AvatarStack";
-import { SidebarIcon } from "@/components/icons/SidebarIcon";
 import { Sidebar } from "@/components/comments/Sidebar";
+import { ToolbarAvatars } from "@/components/comments/ToolbarAvatars";
 
 export function Toolbar({ ...props }) {
   const [open, setOpen] = useState(true);
 
   return (
     <Collapsible.Root open={open} onOpenChange={setOpen} {...props}>
-      <div className={styles.Toolbar}>
-        <div className={styles.ToolbarActions}>
+      <div className={styles.toolbar}>
+        <div className={styles.toolbarActions}>
           <NewThread>
             <Button variant="ghost" square>
               <Plus width={12} height={12} />
@@ -27,48 +26,16 @@ export function Toolbar({ ...props }) {
             </Button>
           </Collapsible.Trigger>
         </div>
-        <div className={styles.ToolbarSeparator} />
-        <div className={styles.ToolbarAvatars}>
-          <img
-            src="https://pbs.twimg.com/profile_images/1401194362927816708/0c3yTtri_400x400.jpg"
-            width="28px"
-            height="28px"
-          />
-          <img
-            src="https://pbs.twimg.com/profile_images/1463879059138617359/QxrFz5rr_400x400.jpg"
-            width="28px"
-            height="28px"
-          />
-        </div>
-        <div className={styles.ToolbarSeparator} />
-        <div className={styles.ToolbarActions}>
+        <div className={styles.toolbarSeparator} />
+        <ToolbarAvatars />
+        <div className={styles.toolbarSeparator} />
+        <div className={styles.toolbarActions}>
           <Button variant="secondary">Save</Button>
           <Button>Publish</Button>
           <Button variant="ghost" square>
             <More width={12} height={12} />
           </Button>
         </div>
-      </div>
-      <Collapsible.Content>
-        <Sidebar onClose={() => setOpen(false)} />
-      </Collapsible.Content>
-    </Collapsible.Root>
-  );
-}
-
-export function ToolbarOld2() {
-  const [open, setOpen] = useState(true);
-
-  return (
-    <Collapsible.Root open={open} onOpenChange={setOpen}>
-      <div className={styles.toolbar}>
-        <NewThread />
-        <AvatarStack />
-        <Collapsible.Trigger asChild>
-          <button style={{ opacity: open ? "0.6" : "1" }}>
-            <SidebarIcon />
-          </button>
-        </Collapsible.Trigger>
       </div>
       <Collapsible.Content>
         <Sidebar onClose={() => setOpen(false)} />
